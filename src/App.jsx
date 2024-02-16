ed63685529cb75137c2c3f96e6afc05ec70a0d2f
codeWithStrange <adebanjofaruq101@gmail.com>
2024-02-16 23:50:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div style={{display: 'flex', justifyContent:'center', alignItems:'center', height: '100vh', fontSize: '34px', padding: '20px'}}>
+      Hey there, this is just a placeholder text, as the portfolio isn't fully completed yet
+    </div>
+  )
+}
+
+export default App
